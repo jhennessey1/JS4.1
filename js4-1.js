5a62bb172c50d4ddf1f05e9374baf3548d582bc0
Student <student@RU-005.local>
2016-01-20 12:29:21 -0700

    part 2 started

--- js4-1.js
+++ js4-1.js
@@ -1,13 +1,23 @@
-angular.module('btnLog', []) 
+// angular.module('btnLog', []) 
 
-angular.module('btnLog').controller('btnLogController', ['$scope', function($scope) {
-	$scope.showMe = false
-	$scope.clickMe = function(){
-		console.log("Pushed!")
-		$scope.showMe = !$scope.showMe
-	}
-	$scope.showMenu = false
-	$scope.clickMenu = function(){
-		$scope.showMenu = !$scope.showMenu
-	}
-}])
+// angular.module('btnLog').controller('btnLogController', ['$scope', function($scope) {
+// 	$scope.showMe = false
+// 	$scope.clickMe = function(){
+// 		console.log("Pushed!")
+// 		$scope.showMe = !$scope.showMe
+// 	}
+// 	$scope.showMenu = false
+// 	$scope.clickMenu = function(){
+// 		$scope.showMenu = !$scope.showMenu
+// 	}
+// }])
+
+angular.module('modDamn' []);
+
+angular.module('modDamn')
+	.controller('modRoller', ['$scope', function($scope) {
+
+		$scope.mousey = function(){
+			
+		}
+	}])
