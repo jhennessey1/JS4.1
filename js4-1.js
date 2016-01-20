afda695080c364d3d58866fdff0b28df6203a9c9
Student <student@RU-005.local>
2016-01-20 08:47:43 -0700

    part 1 complete

--- js4-1.js
+++ js4-1.js
@@ -1,5 +1,13 @@
 angular.module('btnLog', []) 
 
 angular.module('btnLog').controller('btnLogController', ['$scope', function($scope) {
-
+	$scope.showMe = false
+	$scope.clickMe = function(){
+		console.log("Pushed!")
+		$scope.showMe = !$scope.showMe
+	}
+	$scope.showMenu = false
+	$scope.clickMenu = function(){
+		$scope.showMenu = !$scope.showMenu
+	}
 }])
